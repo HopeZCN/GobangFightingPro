85df8404b1b5b13842fa1ce3231fa55c0413e285
HopeCNZ <121518985@qq.com>
2015-12-25 09:39:03 +0800

    add docs

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-var events = require ('events');
+var events = require('events');
 var eventEmitter = new events.EventEmitter();
-var connectHandler = function() {
-    console.log('connecting success!');
-    eventEmitter.emit('dataReceive');
+var listener1 = function() {
+    console.log('listener1 has executed!');
 }
-eventEmitter.on('connect',connectHandler);
-eventEmitter.on('dataReceive',function() {
-    console.log('data receiving success!');
-});
-eventEmitter.emit('connect');
-console.log('do well!');
+var listener2 = function() {
+    console.log('listener2 has executed!');
+}
+eventEmitter.on('connection',listener1);
+eventEmitter.addListener('connection',listener2);
+var eventListeners = events.EventEmitter.listenerCount(eventEmitter,'connection');
+console.log('The number of listeners is ' + eventListeners);
+eventEmitter.emit('connection');
+eventEmitter.removeListener('connection',listener1);
+console.log('remove listener1');
+eventEmitter.emit('connection');
+eventListeners = events.EventEmitter.listenerCount(eventEmitter,'connection');
+console.log('The number of listeners is ' + eventListeners);
+console.log('All finished!');
